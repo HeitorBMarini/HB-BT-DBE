fdab519a76ef745abd0f6f46b05fd6075d2537e5
HeitorBMarini <mariniheitor@gmail.com>
2023-10-10 17:34:46 -0300

    Atualizando projeto

--- src/main/java/br/com/fiap/epictask/task/TaskService.java
+++ src/main/java/br/com/fiap/epictask/task/TaskService.java
@@ -1,27 +1,13 @@
-package br.com.fiap.epictask.task;
+package br.com.fiap.epictask;
 
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+@SpringBootApplication
+public class EpictaskApplication {
 
-@Service
-public class TaskService {
+	public static void main(String[] args) {
+		SpringApplication.run(EpictaskApplication.class, args);
+	}
 
-    @Autowired
-    TaskRepository repository;
-
-    public List<Task> findAll(){
-        return repository.findAll();
-    }
-
-    public boolean delete(Long id) {
-        var task = repository.findById(id);
-
-        if(task.isEmpty()) return false;
-
-        repository.deleteById(id);
-        return true;
-    }
-    
 }
